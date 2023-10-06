6baeb5469b0f5e0ec044db87a2907542462daa8a
fateevanastusha <anastasiafateeva2406@gmail.com>
2023-10-06 13:37:39 +0300

    init react app

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+function Content() {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://c.files.bbci.co.uk/17253/production/_127930849_legolakedisrict.jpg"/>
+            </div>
+            <div>
+                <img src="https://avatars.dzeninfra.ru/get-zen_doc/1889318/pub_5e18feaeb477bf00ae04bfe7_5e18ff22bb892c00af8e61e3/scale_1200"/>
+            </div>
+            <div>
+                My Post
+                <div>
+                    New Post
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Content
